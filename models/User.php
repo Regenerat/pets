f518d031d6859023292145d2807b8fd5f8009f7b
Megira <regenerat.55@mail.ru>
2024-03-04 14:25:06 +0300

    расширил класс User от IdentityInterface
    добавил валидацию
    убрал лишние поля

--- models/User.php
+++ models/User.php
@@ -16,7 +16,7 @@ use Yii;
  * @property PetRequests[] $petRequests
  * @property Role $role
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -32,8 +32,9 @@ class User extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['name', 'email', 'password', 'role_id'], 'required'],
+            [['name', 'email', 'password'], 'required'],
             [['role_id'], 'integer'],
+            ['email', 'email'],
             [['name'], 'string', 'max' => 511],
             [['email', 'password'], 'string', 'max' => 255],
             [['email'], 'unique'],
@@ -73,5 +74,56 @@ class User extends \yii\db\ActiveRecord
     public function getRole()
     {
         return $this->hasOne(Role::class, ['id' => 'role_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id'=> $id]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return null;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
     }
 }
