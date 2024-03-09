df72328c24ef59c7c6d77a44ef0412754e7ecbb6
Megira <regenerat.55@mail.ru>
2024-03-09 17:07:24 +0300

    добавил блок о нас на главную страницу

--- views/site/index.php
+++ views/site/index.php
@@ -5,49 +5,24 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+    <div class="home-banner">
+        <picture>
+            <source srcset="/web/image/banner.webp" type="image/webp">
+            <img class="home-banner__bg" src="/web/image/banner.webp">
+        </picture>
+        <div class="container">
+            <div class="home-banner__body">
+                <div class="home-banner__title">
+                   <h1>Поиск животных</h1>
+                   <span>среди 24 700 объявлений</span>
+                </div>
+                <div class="home-banner__note">
+                    Добро пожаловать на наш портал, посвященный поиску пропавших домашних любимцев! Мы - команда с большим сердцем, создавшая это важное пространство, чтобы помочь семьям в трудный момент – когда любимый питомец исчезает.
+                </div>
+                <div class="home-banner__note">
+                Мы понимаем, что ваш питомец – часть вашей семьи, и его потеря может быть тяжелым испытанием. Наш ресурс предоставляет простой и быстрый способ сообщить о пропаже, распространить информацию в сети, а также получить поддержку от других владельцев, которые понимают ваши чувства.
+                </div>
             </div>
         </div>
-
-    </div>
+    </div>   
 </div>
