6ea294d0286665b64755d7cb9575f0ca874f1057
Megira <regenerat.55@mail.ru>
2024-03-09 17:45:50 +0300

    добавил блок преимуществ

--- views/site/index.php
+++ views/site/index.php
@@ -24,5 +24,81 @@ $this->title = 'My Yii Application';
                 </div>
             </div>
         </div>
-    </div>   
+    </div>
+
+    <div class="container">
+        <div class="jumbotron text-left bg-transparent mt-5 mb-5">
+            <h1 class="display-4">Наши преимущества</h1>
+        </div>
+    </div>
+    
+    <div class="col-sm-12">
+        <div class="container">
+            <div class="row">
+                <div class="col br-block-green mt-5 px-4 py-2 g-0 g-sm-5">
+                    <div class="row">
+                        <div class="col-3 my-auto">
+                            <img src="web\image\1.png" class="img-fluid rounded">
+                        </div>
+                        <div class="col-9 my-auto">
+                            <p class="fs-5 d-flex justify-content-left fw-semibold mb-0">Быстрый и эффективный поиск</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col br-block-green mt-5 px-4 py-2 ms-5 g-0 g-sm-5">
+                    <div class="row">
+                        <div class="col-3 my-auto">
+                            <img src="web\image\2.png" class="img-fluid rounded">
+                        </div>
+                        <div class="col-9 my-auto">
+                            <p class="fs-5 d-flex justify-content-left fw-semibold mb-0">Сетевая поддержка</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col br-block-green mt-5 px-4 py-2 ms-5 g-0 g-sm-5">
+                    <div class="row">
+                        <div class="col-3 my-auto">
+                            <img src="web\image\3.png" class="img-fluid rounded">
+                        </div>
+                        <div class="col-9 my-auto">
+                            <p class="fs-5 d-flex justify-content-left fw-semibold mb-0 word-wrap:break-word">Информированность и уведомления</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col br-block-green mt-5 px-4 py-2 g-0 g-sm-5">
+                    <div class="row">
+                        <div class="col-3 my-auto">
+                            <img src="web\image\4.png" class="img-fluid rounded">
+                        </div>
+                        <div class="col-9 my-auto">
+                            <p class="fs-5 d-flex justify-content-left fw-semibold mb-0">Безопасность и конфиденциальность</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col br-block-green mt-5 px-4 py-2 ms-5 g-0 g-sm-5">
+                    <div class="row">
+                        <div class="col-3 my-auto">
+                            <img src="web\image\5.png" class="img-fluid rounded">
+                        </div>
+                        <div class="col-9 my-auto">
+                            <p class="fs-5 d-flex justify-content-left fw-semibold mb-0">Поддержка владельцев</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col br-block-green mt-5 px-4 py-2 ms-5 g-0 g-sm-5">
+                    <div class="row">
+                        <div class="col-3 my-auto">
+                            <img src="web\image\6.png" class="img-fluid rounded">
+                        </div>
+                        <div class="col-9 my-auto">
+                            <p class="fs-5 d-flex justify-content-left fw-semibold mb-0">Современные технологии</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
