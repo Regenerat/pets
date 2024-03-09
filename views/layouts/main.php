451a30f39d29623280e1d1e571d6f1d32694d7a1
Megira <regenerat.55@mail.ru>
2024-03-09 17:06:57 +0300

    содержимое страницы больше не находится внутри container

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -67,14 +67,12 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     ?>
 </header>
 
-<main id="main" class="flex-shrink-0" role="main">
-    <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
+<main id="main" class="flex-shrink-0" role="main">   
+    <?php if (!empty($this->params['breadcrumbs'])): ?>
+        <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
+    <?php endif ?>
+    <?= Alert::widget() ?>
+    <?= $content ?>
 </main>
 
 <footer id="footer" class="mt-auto py-3 bg-light">
